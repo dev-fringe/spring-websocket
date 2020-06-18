8298d7250193598769678b5c62337f73acebaefe
Lee Hak Do <dev.fringe@gmail.com>
2020-06-18 09:49:31 +0900

    init

---
+++ src/main/java/dev/fringe/config/WebSocketServlet.java
@@ -0,0 +1,47 @@
+package dev.fringe.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import com.github.mjeanroy.springmvc.view.mustache.configuration.EnableMustache;
+import com.github.mjeanroy.springmvc.view.mustache.configuration.MustacheProvider;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@EnableWebMvc
+@EnableMustache(provider = MustacheProvider.AUTO)
+@ComponentScan({ "dev.fringe.controller"})
+public class WebSocketServlet extends AbstractAnnotationConfigDispatcherServletInitializer implements WebSocketMessageBrokerConfigurer {
+
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { WebSocketServlet.class};
+	}
+
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	//below implements WebSocketMessageBrokerConfigurer
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app");
+	}
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/connectWS"); 
+		registry.addEndpoint("/connectWS").withSockJS(); 
+	}
+
+}
