b95ffa25bccc6191866a5d6de297ace82152bd95
Lee Hak Do <dev.fringe@gmail.com>
2020-06-18 09:47:42 +0900

    init

--- src/main/java/dev/fringe/controller/WebSocketController.java
+++ src/main/java/dev/fringe/controller/WebSocketController.java
@@ -3,15 +3,21 @@ package dev.fringe.controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import dev.fringe.model.Add;
 
 @Controller
 public class WebSocketController {
 
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+	
 	@MessageMapping("/add")
 	@SendTo("/topic/response")
-	public Add addNum(Add req) throws Exception {
-		return new Add(req.getOne() + "+" + req.getTwo() + "=" + (req.getOne() + req.getTwo()));
+	public Add add(Add req) {
+		return new Add(String.format("%s+%s=%s", req.getOne(),req.getTwo(), req.getOne() + req.getTwo()));
 	}
 }
--- src/main/java/dev/fringe/model/Add.java
+++ src/main/java/dev/fringe/model/Add.java
@@ -1,18 +1,18 @@
 package dev.fringe.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Add {
 	private int one;
 	private int two;
 	
 	private String result;
 
-	public Add() {
-		super();
-	}
-	
 	public Add(String result) {
 		this.result = result;
 	}
